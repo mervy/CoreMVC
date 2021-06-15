a119a5dd3c0233da6a0fa03cbda1869828a970d5
mervy <rgrsoares@yahoo.com.br>
2021-06-15 10:17:02 -0300

    Database mais simples

--- database.sql
+++ database.sql
@@ -1,6 +1,24 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 14-Jun-2021 às 19:22
+-- Versão do servidor: 8.0.21
+-- versão do PHP: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `coremvc_1`
+-- Banco de dados: `coremvc`
 --
 
 -- --------------------------------------------------------
@@ -12,10 +30,10 @@
 DROP TABLE IF EXISTS `articles`;
 CREATE TABLE IF NOT EXISTS `articles` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(128) NOT NULL,
-  `image` varchar(128) NOT NULL,
-  `content` text NOT NULL,
-  `status` char(3) NOT NULL DEFAULT 'not' COMMENT 'yes or not',
+  `title` varchar(128) COLLATE utf8mb4_general_ci NOT NULL,
+  `image` varchar(128) COLLATE utf8mb4_general_ci NOT NULL,
+  `content` mediumtext COLLATE utf8mb4_general_ci NOT NULL,
+  `status` char(3) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'not' COMMENT 'yes or not',
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
   `authors_id` int NOT NULL,
@@ -23,7 +41,9 @@ CREATE TABLE IF NOT EXISTS `articles` (
   PRIMARY KEY (`id`),
   KEY `fk_articles_authors_idx` (`authors_id`),
   KEY `fk_articles_categories1_idx` (`categories_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
 -- Estrutura da tabela `authors`
@@ -32,19 +52,19 @@ CREATE TABLE IF NOT EXISTS `articles` (
 DROP TABLE IF EXISTS `authors`;
 CREATE TABLE IF NOT EXISTS `authors` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nickname` varchar(32) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `email` varchar(64) NOT NULL,
-  `password` varchar(60) NOT NULL,
-  `status` char(3) NOT NULL DEFAULT 'yes' COMMENT 'yes or not',
+  `nickname` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(128) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(64) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(60) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` char(3) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'yes' COMMENT 'yes or not',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `nickname` (`nickname`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `authors`
--- Password: 12345
+-- Senha inicial: 12345
 
 INSERT INTO `authors` (`id`, `nickname`, `name`, `email`, `password`, `status`) VALUES
 (1, 'admin', 'Administrador', 'admin@admin.com', '$2y$10$GgJCickfqNzbnuZS4sfPO.BUry85IRRm1BI/vWMD6o9wJK0glOvqm', 'yes');
@@ -58,12 +78,12 @@ INSERT INTO `authors` (`id`, `nickname`, `name`, `email`, `password`, `status`) 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) NOT NULL,
-  `slug` varchar(128) NOT NULL,
-  `description` varchar(512) NOT NULL,
+  `name` varchar(128) COLLATE utf8mb4_general_ci NOT NULL,
+  `slug` varchar(128) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(512) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -74,13 +94,13 @@ CREATE TABLE IF NOT EXISTS `categories` (
 DROP TABLE IF EXISTS `newsletters`;
 CREATE TABLE IF NOT EXISTS `newsletters` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) NOT NULL,
-  `email` varchar(64) NOT NULL,
-  `ip` varchar(15) NOT NULL,
+  `name` varchar(128) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(64) COLLATE utf8mb4_general_ci NOT NULL,
+  `ip` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
   `registered_in` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -92,12 +112,11 @@ DROP TABLE IF EXISTS `visitors`;
 CREATE TABLE IF NOT EXISTS `visitors` (
   `id` int NOT NULL AUTO_INCREMENT,
   `articles_id` int NOT NULL,
-  `ip` varchar(20) NOT NULL,
+  `ip` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
   `acessed_in` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_visitors_articles_id` (`articles_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Restrições para despejos de tabelas
@@ -116,3 +135,7 @@ ALTER TABLE `articles`
 ALTER TABLE `visitors`
   ADD CONSTRAINT `fk_visitors_articles` FOREIGN KEY (`articles_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
